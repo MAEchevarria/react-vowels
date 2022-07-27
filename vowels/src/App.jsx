6776335864ac7a993c89ca09951209fb45b51967
Michael Echevarria <michael.echevarria@live.com>
2022-07-26 19:47:10 -0500

    functional user input

--- vowels/src/App.jsx
+++ vowels/src/App.jsx
@@ -1,34 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [word, setWord] = useState('');
+  // use [] to track word history ?
+
+  const handleSubmit = () => {
+    let userInput = document.getElementById('user-input')
+    if (userInput) {
+      // need .value
+      setWord(userInput.value)
+    }
+  }
+  // using a form for submit
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   alert(`The word you entered was: ${word}`);
+  // }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h2>Enter a word. Any word</h2>
+      <input id='user-input' type="text" placeholder='Enter word' />
+      <button onClick={handleSubmit}>Submit</button>
+      <p>The submitted word is: {word}</p>
     </div>
-  )
+    // <div>
+    //   <form onSubmit={handleSubmit}>
+    //     <label>Enter a word: 
+    //       <input type="text" value={word} onChange={(e) => setWord(e.target.value)}/>
+    //     </label>
+    //     <input type="submit"/>
+    //   </form>
+    // </div>
+  );
 }
 
 export default App
